507d7116968c2c244c7db52110f7c11ccb4216d2
Felix Seidl <Felix.Seidl@conplement.de>
2019-10-16 16:07:26 +0200

    Integrate calinga i18next backend package

--- App.js
+++ App.js
@@ -7,67 +7,65 @@
  */
 
 import React, { Fragment, useEffect, useState } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Picker,
-} from 'react-native';
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import AsyncStorage from '@react-native-community/async-storage';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Picker, Button } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { useTranslation } from 'react-i18next';
 
-const App = () => {
-  const [t, i18n] = useTranslation("", { useSuspense: false });
-  [language, setLanguage] = useState("de");
-  useEffect(() => {
-    i18n.changeLanguage(language);
-  }, [language]);
+const clearStore = async () => {
+    await AsyncStorage.clear();
+};
 
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={ styles.scrollView }>
-          <View style={ styles.body }>
-            <View style={ styles.sectionContainer }>
-              <View style={ styles.sectionTitle }>
-                <Text>{ t("Login.Title") }</Text>
-              </View>
-            </View>
-            <Picker selectedValue={ language } onValueChange={ value => setLanguage(value) }>
-              <Picker.Item label="Deutsch" value="de" />
-              <Picker.Item label="English" value="en" />
-            </Picker>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
+const App = () => {
+    const [t, i18n, ready] = useTranslation(undefined, { useSuspense: false });
+    [language, setLanguage] = useState(i18n.lng);
+    useEffect(() => {
+        i18n.changeLanguage(language);
+    }, [language]);
+
+    if (!ready) {
+        return <Text>Loading...</Text>;
+    }
+
+    return (
+        <Fragment>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView>
+                <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+                    <View style={styles.body}>
+                        <View style={styles.sectionContainer}>
+                            <View style={styles.sectionTitle}>
+                                <Text>{t('Login.Title')}</Text>
+                            </View>
+                        </View>
+                        <Picker selectedValue={language} onValueChange={value => setLanguage(value)}>
+                            <Picker.Item label="English" value="en" />
+                            <Picker.Item label="Deutsch" value="de" />
+                        </Picker>
+                    </View>
+                    <Button title="Clear translations" onPress={() => clearStore()} />
+                </ScrollView>
+            </SafeAreaView>
+        </Fragment>
+    );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  }
+    scrollView: {
+        backgroundColor: Colors.lighter
+    },
+    body: {
+        backgroundColor: Colors.white
+    },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+        color: Colors.black
+    }
 });
 
 export default App;
